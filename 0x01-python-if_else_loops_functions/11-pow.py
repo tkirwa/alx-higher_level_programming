ffb6e72b63fb8766b4d41042c97c1cc28dfc472e
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-07 10:17:18 -0800

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    if b == 0:
-        return 1
-    elif b < 0:
-        a = 1 / a
-        b = -b
-    result = a
-    for i in range(b - 1):
-        result *= a
-    return result
+    return (a ** b)
