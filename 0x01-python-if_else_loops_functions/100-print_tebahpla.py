5d9f7d9fcf464380308f803340788f6deca57d58
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-07 12:18:35 -0800

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python3
 
-ascii_val = ord('z')
-counter = 0
-
-while ascii_val >= ord('A'):
-    if counter % 2 == 0:
-        print("{:c}".format(ascii_val), end="")
-    else:
-        print("{:c}".format(ascii_val).upper(), end="")
-    ascii_val -= 1
-    counter += 1
+for c in range(90, 64, -1):
+    print("{:c}".format(c + 32 * ((c % 2) ^ 1)), end="")
