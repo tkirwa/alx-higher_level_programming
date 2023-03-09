525286abd1106969dc62fb9157606a65f98e6087
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-09 12:14:47 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
-import sys
+
+# 3-infinite_add.py
 
 if __name__ == "__main__":
+
+    """Print the addition of all arguments."""
+    
+    import sys
+
     args = sys.argv[1:]
     result = 0
     for arg in args:
