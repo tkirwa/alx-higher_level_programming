40a930d13a8fca5d49099f50b5a4d797c10e26b7
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-03-27 09:14:21 -0700

    101-safe_function.py - Write a function that executes a function safely.

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""
+safe_function module
+
+Contains function that executes a function safely
+"""
+
 import sys
+
 
 def safe_function(fct, *args):
     try:
         result = fct(*args)
         return result
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
+    except Exception as error:
+        print("Exception: {}".format(error), file=sys.stderr)
         return None
