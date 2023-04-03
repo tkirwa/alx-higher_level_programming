3c43de80af72d652fbf1f8b779b18be92ed65bbb
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-03 14:43:46 -0700

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,65 +1,44 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
 
 
-def is_valid(board, row, col, n):
-    """
-    Check if it is valid to place a queen at board[row][col]
-    given the current state of the board
-    """
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
+def nqueens(n):
+    if n < 4:
+        print("N must be at least 4")
+        return
 
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+    rows = set()
+    cols = set()
+    diagonals1 = set()
+    diagonals2 = set()
 
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+    def backtrack(queens, row):
+        if row == n:
+            print(queens)
+            return
+        for col in range(n):
+            if col in cols or row + col in diagonals1 or row - col in diagonals2:
+                continue
+            rows.add(row)
+            cols.add(col)
+            diagonals1.add(row + col)
+            diagonals2.add(row - col)
+            backtrack(queens + [[row, col]], row + 1)
+            rows.remove(row)
+            cols.remove(col)
+            diagonals1.remove(row + col)
+            diagonals2.remove(row - col)
 
-    return True
-
-
-def solve(board, col, n):
-    """
-    Solve N Queen problem
-    """
-    if col >= n:
-        # All queens are placed, print the solution
-        print([[i, j] for i in range(n) for j in range(n) if board[i][j] == 1])
-        return True
-
-    for i in range(n):
-        if is_valid(board, i, col, n):
-            # Place the queen at board[i][col]
-            board[i][col] = 1
-
-            # Recur to place rest of the queens
-            solve(board, col + 1, n)
-
-            # If placing queen in board[i][col] doesn't lead
-            # to a solution, backtrack
-            board[i][col] = 0
-
-    return False
+    backtrack([], 0)
 
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-
     try:
         n = int(sys.argv[1])
     except ValueError:
         print("N must be a number")
         sys.exit(1)
-
-    if n < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [[0 for x in range(n)] for y in range(n)]
-    solve(board, 0, n)
+    nqueens(n)
