cebe81635e979c58f396809296e0310817e2e4be
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-12 10:33:55 -0700

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,36 +1,42 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import sys
+import signal
 
+def print_stats(total_file_size, status_codes):
+    print("File size: {}".format(total_file_size))
+    for k in sorted(status_codes.keys()):
+        if status_codes[k]:
+            print("{}: {}".format(k, status_codes[k]))
 
-def print_stats(file_size, status_codes):
-    """Prints the current stats"""
-    print("File size: {}".format(file_size))
-    for code in sorted(status_codes.keys()):
-        if status_codes[code] != 0:
-            print("{}: {}".format(code, status_codes[code]))
+def parse_line(line):
+    parts = line.split()
+    return (int(parts[-2]), int(parts[-1]))
 
+def main():
+    total_file_size = 0
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    line_count = 0
 
-def parse_line(line, status_codes):
-    """Parses a log line and updates the status codes and file size"""
-    try:
-        line_parts = line.split()
-        status = line_parts[-2]
-        if status in status_codes:
-            status_codes[status] += 1
-        file_size = int(line_parts[-1])
-    except:
-        return 0
-    return file_size
+    def signal_handler(signal, frame):
+        print_stats(total_file_size, status_codes)
+        sys.exit(0)
+    signal.signal(signal.SIGINT, signal_handler)
 
+    for line in sys.stdin:
+        line_count += 1
+        try:
+            status_code, file_size = parse_line(line)
+            total_file_size += file_size
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except:
+            pass
+
+        if line_count == 10:
+            print_stats(total_file_size, status_codes)
+            line_count = 0
+
+    print_stats(total_file_size, status_codes)
 
 if __name__ == "__main__":
-    file_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-    try:
-        for i, line in enumerate(sys.stdin, 1):
-            file_size += parse_line(line, status_codes)
-            if i % 10 == 0:
-                print_stats(file_size, status_codes)
-    except KeyboardInterrupt:
-        print_stats(file_size, status_codes)
-        raise
+    main()
