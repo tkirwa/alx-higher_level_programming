07728b4a1f686f8267af22d87a640fab5eada26f
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-12 10:06:18 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-# 7-save_to_json_file.py
-"""Defines a JSON file-writing function."""
+
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    """Write an object to a text file using JSON representation."""
+    """function that writes an Object to a text file, using a JSON"""
     with open(filename, "w") as f:
         json.dump(my_obj, f)
