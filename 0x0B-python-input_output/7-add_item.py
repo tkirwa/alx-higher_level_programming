59c30ace51839e1f80c4eac0328ca1b10ac278fe
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-04-12 09:55:05 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
+"""Add all arguments to a Python list and save them to a file"""
+
 import sys
+import json
+from os import path
+from typing import List
+
+
+def load_from_json_file(filename: str) -> List:
+    """Load JSON data from a file"""
+    with open(filename, "r") as f:
+        return json.load(f)
+
+
+def save_to_json_file(my_obj: List, filename: str) -> None:
+    """Save JSON data to a file"""
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
+
 
 if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
-
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+    filename = "add_item.json"
+    args = sys.argv[1:]
+    if path.exists(filename):
+        my_list = load_from_json_file(filename)
+    else:
+        my_list = []
+    my_list += args
+    save_to_json_file(my_list, filename)
