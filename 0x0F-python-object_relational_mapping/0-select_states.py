a951af3552d678c77e884c5a4107993ca55a53bb
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-17 12:40:12 +0300

    fix - hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,26 +14,12 @@ if __name__ == "__main__":
     database = sys.argv[3]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password)
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, db=database
+    )
 
     # Create a cursor object to interact with the database
     cursor = db.cursor()
-
-    # Create the database if it doesn't exist
-    cursor.execute("CREATE DATABASE IF NOT EXISTS {}".format(database))
-
-    # Select the database
-    cursor.execute("USE {}".format(database))
-
-    # Create the 'states' table if it doesn't exist
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS states (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY (id))"
-    )
-
-    # Insert sample data into the 'states' table
-    cursor.execute(
-        "INSERT INTO states (name) VALUES ('California'), ('Arizona'), ('Texas'), ('New York'), ('Nevada')"
-    )
 
     # Execute the query to retrieve all states
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
