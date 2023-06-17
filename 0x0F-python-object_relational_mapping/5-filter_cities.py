a4908dd6df58c60a48d5a2f63baa15024d1dcb75
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-17 15:13:10 +0300

    5-filter_cities.py fix

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,10 +3,11 @@
 Script that lists all cities of a given state from the database hbtn_0e_4_usa
 """
 
-import MySQLdb
 import sys
 
-if __name__ == '__main__':
+import MySQLdb
+
+if __name__ == "__main__":
     # Get command-line arguments
     username = sys.argv[1]
     password = sys.argv[2]
@@ -14,28 +15,29 @@ if __name__ == '__main__':
     state_name = sys.argv[4]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
     # Create a cursor object to interact with the database
     cursor = db.cursor()
 
     # Execute the query to retrieve cities of the given state
-    cursor.execute("SELECT cities.name \
-                    FROM cities JOIN states \
-                    ON cities.state_id = states.id \
-                    WHERE states.name = %s \
-                    ORDER BY cities.id ASC", (state_name,))
+    cursor.execute("SELECT * FROM `cities` as `c` \
+                   INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                   ORDER BY `c`.`id`"
+    )
 
     # Fetch all the rows returned by the query
     cities = cursor.fetchall()
 
     # Print the cities
-    if cities:
-        print(", ".join(city[0] for city in cities))
+    print(", ".join([ct[2] for ct in cursor.fetchall() if ct[4] == sys.argv[4]]))
 
     # Close cursor and database connection
     cursor.close()
