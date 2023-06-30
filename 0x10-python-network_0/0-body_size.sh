2d0afc497b09b1507e3925624c01f135ad142156
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-30 02:00:06 -0700

    0-body_size.sh wc fix

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,20 +1,4 @@
 #!/bin/bash
-# Sends a request to a URL and displays the size of the response body
+# Sends a request to a URL and displays the size of the response body in bytes
 
-# Get the URL from the command-line argument
-url=$1
-
-# Send the request and store the response in a variable
-response=$(curl -sI "$url")
-
-# Extract the Content-Length header value from the response
-content_length=$(echo "$response" | grep -i Content-Length | awk '{print $2}')
-
-# Send a GET request to the URL and retrieve the response body
-body=$(curl -s "$url")
-
-# Calculate the size of the response body in bytes
-body_size=$(echo -n "$body" | wc -c)
-
-# Display the content length (size) in bytes
-echo "$body_size"
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
