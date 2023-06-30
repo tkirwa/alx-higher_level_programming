7fdace5445056968cfef15c22ed92b45fe1af398
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-30 22:19:23 +0300

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
 Sends a POST request to a URL with an email as a parameter and...
- displays the body of the response.
+displays the body of the response.
 """
 
 import requests
 import sys
 
 if __name__ == "__main__":
-    """Send a POST request with email as a parameter and display...
-      the response body."""
     url = sys.argv[1]
     email = sys.argv[2]
-    payload = {'email': email}
 
+    payload = {'email': email}
     response = requests.post(url, data=payload)
+
     print("Your email is:", response.text)
