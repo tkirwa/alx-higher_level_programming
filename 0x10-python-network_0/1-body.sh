be1c64c6ebfb63a7ca464f8a229444b4594ff2de
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-06-30 02:07:45 -0700

    1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL and displays the body of the response (if status code is 200)
-
+# Get the response body for a given URL for 200 status code responses.
 curl -sL -w "%{http_code}" "$1" -o /tmp/body | tail -n 1 | grep -q 200 && cat /tmp/body
