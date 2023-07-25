5452cc1bf0c53048242984b49dd96c064eb42f8a
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-25 23:38:26 +0300

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,28 +1,36 @@
 #!/usr/bin/node
+// Prints the number of movies where "Wedge Antilles" is present
 
 const request = require('request');
+const url = process.argv[2]; // Get the API URL from the command-line arguments
 
-// Get the API URL from the command-line arguments
-const apiUrl = process.argv[2];
-
-// Wedge Antilles character ID
-const wedgeAntillesId = 18;
-
-// Send a GET request to the Star Wars API
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
+request(url, function (err, res, body) {
+  if (err) {
     // If an error occurred during the request, print the error object
-    console.error(error);
+    console.log(err);
+  } else if (res.statusCode === 200) {
+    // If the request was successful (status code 200), proceed
+
+    // Parse the response body as JSON and access the "results" array
+    const films = JSON.parse(body).results;
+
+    let count = 0; // Initialize the count to 0
+
+    // Iterate through each movie
+    for (let i = 0; i < films.length; i++) {
+      // Iterate through each character in the movie
+      for (let j = 0; j < films[i].characters.length; j++) {
+        // Check if the character includes the ID for "Wedge Antilles" (ID: 18)
+        if (films[i].characters[j].includes('/18/')) {
+          // If found, increment the count
+          count++;
+        }
+      }
+    }
+    // Print the final count of movies where "Wedge Antilles" is present
+    console.log(count);
   } else {
-    // Parse the response body as JSON to access the movie data
-    const movieData = JSON.parse(body);
-
-    // Filter the movie data to find the movies where Wedge Antilles is present
-    const moviesWithWedgeAntilles = movieData.results.filter(movie =>
-      movie.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)
-    );
-
-    // Print the number of movies where Wedge Antilles is present
-    console.log(moviesWithWedgeAntilles.length);
+    // If the request returned an invalid response, print 'Invalid'
+    console.log('Invalid');
   }
 });
