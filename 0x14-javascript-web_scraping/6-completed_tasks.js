248cb129decdf4130452dd412fd21c7d1bcaab0b
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-25 23:51:25 +0300

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,35 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
-const apiUrl = process.argv[2];
+const url = process.argv[2];
 
-// Send a GET request to the specified API URL
-request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    // If an error occurred during the request, print the error object
-    console.error(error);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else if (response.statusCode === 200) {
-    // If the request was successful (status code 200), proceed
-
-    // Parse the response body as JSON to access the tasks data
+    const completed = {};
     const tasks = JSON.parse(body);
-
-    // Create an empty object to store the number of completed tasks per user
-    const completedTasks = {};
-
-    // Loop through each task
-    for (const task of tasks) {
-      // Check if the task is completed (completed tasks have 'completed' property set to true)
-      if (task.completed) {
-        // Increment the count for the corresponding user ID or set it to 1 if the ID doesn't exist in the object
-        completedTasks[task.userId] = (completedTasks[task.userId] || 0) + 1;
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
       }
     }
-
-    // Print the object containing the number of completed tasks per user ID
-    console.log(completedTasks);
+    console.log(completed);
   } else {
-    // If the request returned an invalid response, print the status code
-    console.log('An error occurred. Status code: ' + response.statusCode);
+    console.log('An error occured. Status code: ' + response.statusCode);
   }
 });
