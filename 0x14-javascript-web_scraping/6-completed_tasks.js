42481894c3a844f4b05f736c1c5331020aa75c61
Tonny Kirwa <tonnykirwa@yahoo.com>
2023-07-25 23:46:46 +0300

    6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,25 +2,16 @@
 // Computes the number of tasks completed by user id from jsonplaceholder.typicode.com API
 
 const request = require('request');
-const url = process.argv[2]; // Get the API URL from the command-line arguments
+const url = process.argv[2];
 
 request(url, function (err, res, body) {
   if (err) {
-    // If an error occurred during the request, print the error object
     console.log(err);
   }
-
-  // Parse the response body as JSON to access the tasks data
-  const tasks = JSON.parse(body);
-
-  // Create an empty object to store the number of completed tasks per user
-  const obj = {};
-
-  // Loop through each task
-  for (const task of tasks) {
-    // Check if the task is completed (completed tasks have 'completed' property set to true)
+  let tasks = JSON.parse(body);
+  let obj = {};
+  for (let task of tasks) {
     if (task.completed === true) {
-      // Increment the count for the corresponding user ID or set it to 1 if the ID doesn't exist in the object
       if (obj[task.userId] === undefined) {
         obj[task.userId] = 1;
       } else {
@@ -28,7 +19,5 @@ request(url, function (err, res, body) {
       }
     }
   }
-
-  // Print the object containing the number of completed tasks per user ID
   console.log(obj);
 });
